8e8deca571f51d340093338b43306a5fd4ebb8f4
GilanRanger <58899528+GilanRanger@users.noreply.github.com>
2021-01-20 15:33:32 -0800

    Update

--- src/main/java/com/mcmiddleearth/mcmemusic/Main.java
+++ src/main/java/com/mcmiddleearth/mcmemusic/Main.java
@@ -1,4 +1,48 @@
+/*
+ * Copyright (C) 2020 MCME
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.mcmiddleearth.mcmemusic;
 
-public class Main {
+import com.mcmiddleearth.mcmemusic.commands.MusicRegionCommand;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public static JavaPlugin instance;
+    public static WorldEditPlugin WEinstance;
+
+    @Override
+    public void onEnable(){
+        instance = this;
+        this.saveDefaultConfig();
+        this.getConfig().options().copyDefaults(true);
+        this.getCommand("musicrg").setExecutor(new MusicRegionCommand());
+    }
+
+    public static WorldEditPlugin getWorldEdit(){
+        Plugin p = Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
+        if(p instanceof WorldEditPlugin) return (WorldEditPlugin)p;
+        return WEinstance;
+    }
+
+    public static JavaPlugin getInstance(){
+        return instance;
+    }
+
 }
