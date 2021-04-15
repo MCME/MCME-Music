87510b86cfaf268a0569a7ba4e67520b88b0c345
GilanRanger <58899528+GilanRanger@users.noreply.github.com>
2021-04-15 10:57:04 -0700

    Added loading cube regions

--- src/main/java/com/mcmiddleearth/mcmemusic/util/LoadRegion.java
+++ src/main/java/com/mcmiddleearth/mcmemusic/util/LoadRegion.java
@@ -6,6 +6,8 @@ import com.mcmiddleearth.mcmemusic.file.JSONFile;
 import com.mcmiddleearth.mcmemusic.data.Region;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.math.BlockVector2;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.regions.Polygonal2DRegion;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
@@ -13,15 +15,17 @@ import org.bukkit.World;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class LoadRegion {
 
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private Main main;
-    private JSONFile jsonFile;
-    private Logger log = Bukkit.getLogger();
-    private HashMap<Polygonal2DRegion, Region> allRegions = new HashMap<>();
+    private final Main main;
+    private final JSONFile jsonFile;
+    private final Logger log = Bukkit.getLogger();
+    private final HashMap<Polygonal2DRegion, Region> polyRegions = new HashMap<>();
+    private final HashMap<CuboidRegion, Region> cubeRegions = new HashMap<>();
 
     public LoadRegion(Main main, JSONFile jsonFile){
         this.main = main;
@@ -31,7 +35,6 @@ public class LoadRegion {
     public void loadRegions() throws Exception {
         File dataFolder = new File(String.valueOf(main.getDataFolder()));
         File[] files = dataFolder.listFiles();
-        allRegions.clear();
 
         for(File f: files){
             if(f.getName().contains(".json")){
@@ -39,8 +42,11 @@ public class LoadRegion {
                 JsonObject json = (JsonObject) jsonFile.readJson(f.getName());
                 JsonPrimitive jsonID = json.getAsJsonPrimitive("musicID");
                 String name = json.get("name").getAsString();
+                String shape = json.get("shape").getAsString();
 
-                //Get points
+                int musicID = jsonID.getAsInt();
+                int weight = jsonID.getAsInt();
+
                 JsonArray points = json.getAsJsonArray("points");
 
                 if(points != null){
@@ -49,54 +55,67 @@ public class LoadRegion {
                     }
                 }
 
-                //Get musicID
-                int musicID = jsonID.getAsInt();
+                if(shape.contains("poly")){
 
-                //Log
-                log.info("Test log");
-                log.info("File:" + f.getName());
-                log.info("Points:" + pointList);
-                log.info("Music ID:" + musicID);
+                    Polygonal2DRegion region = new Polygonal2DRegion();
 
-                //Create Region
-                Polygonal2DRegion region = new Polygonal2DRegion();
+                    for(int i=0;i<pointList.size();i++){
+                        int firstIndex = pointList.get(i).indexOf(":");
+                        int lastIndex = pointList.get(i).lastIndexOf(":");
+                        int secondIndex = pointList.get(i).indexOf(":", firstIndex + 1);
+                        String world = pointList.get(i).substring(1, firstIndex);
+                        String stringX = pointList.get(i).substring(firstIndex+1, secondIndex);
+                        String stringZ = pointList.get(i).substring(lastIndex+1, pointList.get(i).length()-1);
 
-                for(int i=0;i<pointList.size();i++){
-                    //Add point region
-                    int x;
-                    int z;
+                        int x = Integer.parseInt(stringX);
+                        int z = Integer.parseInt(stringZ);
+                        World bukkitWorld = Bukkit.getServer().getWorld(world);
+                        com.sk89q.worldedit.world.World regionWorld = BukkitAdapter.adapt(bukkitWorld);
+                        BlockVector2 pointLoc = BlockVector2.at(x,z);
 
-                    //Deserialize
-                    int firstIndex = pointList.get(i).indexOf(":");
-                    int lastIndex = pointList.get(i).lastIndexOf(":");
-                    int secondIndex = pointList.get(i).indexOf(":", firstIndex + 1);
-                    String world = pointList.get(i).substring(1, firstIndex);
-                    String stringX = pointList.get(i).substring(firstIndex+1, secondIndex);
-                    String stringZ = pointList.get(i).substring(lastIndex+1, pointList.get(i).length()-1);
+                        region.setWorld(regionWorld);
+                        region.addPoint(pointLoc);
+                    }
+                    polyRegions.put(region, new Region(name, pointList, musicID, weight, shape));
+                }
+                else if(shape.contains("cube")){
+                    
+                    List<BlockVector3> positions = new ArrayList<>();
+                    String world = null;
 
-                    log.info(world);
+                    for(int i=0;i<pointList.size();i++){
+                        int firstIndex = pointList.get(i).indexOf(":");
+                        int lastIndex = pointList.get(i).lastIndexOf(":");
+                        int secondIndex = pointList.get(i).indexOf(":", firstIndex + 1);
+                        world = pointList.get(i).substring(1, firstIndex);
+                        String stringX = pointList.get(i).substring(firstIndex+1, secondIndex);
+                        String stringY = pointList.get(i).substring(secondIndex+1, lastIndex);
+                        String stringZ = pointList.get(i).substring(lastIndex+1, pointList.get(1).length()-1);
 
-                    //Set variables to final values
-                    x = Integer.parseInt(stringX);
-                    z = Integer.parseInt(stringZ);
+                        int x = Integer.parseInt(stringX);
+                        int y = Integer.parseInt(stringY);
+                        int z = Integer.parseInt(stringZ);
+
+                        BlockVector3 pos = BlockVector3.at(x, y, z);
+                        positions.add(pos);
+                    }
+                    CuboidRegion region = new CuboidRegion(positions.get(0), positions.get(1));
+
                     World bukkitWorld = Bukkit.getServer().getWorld(world);
                     com.sk89q.worldedit.world.World regionWorld = BukkitAdapter.adapt(bukkitWorld);
-                    BlockVector2 pointLoc = BlockVector2.at(x,z);
-
-                    //Add Point and Set World
                     region.setWorld(regionWorld);
-                    region.addPoint(pointLoc);
+
+                    cubeRegions.put(region, new Region(name, pointList, musicID, weight, shape));
                 }
-
-                allRegions.put(region, new Region(name, pointList, musicID));
-
             }
         }
-        log.info(String.valueOf(allRegions));
     }
 
-    public HashMap<Polygonal2DRegion, Region> getRegionsMap(){
-        return allRegions;
+    public HashMap<Polygonal2DRegion, Region> getPolyRegionsMap(){
+        return polyRegions;
+    }
+    public HashMap<CuboidRegion, Region> getCubeRegionsMap(){
+        return cubeRegions;
     }
 
 }
