0f6303760c4cf9299839f8e80a4d7db3b6dd2ef6
GilanRanger <58899528+GilanRanger@users.noreply.github.com>
2021-04-15 10:51:00 -0700

    Fixed Region reload for cuboid regions

--- src/main/java/com/mcmiddleearth/mcmemusic/commands/MusicRegionCommand.java
+++ src/main/java/com/mcmiddleearth/mcmemusic/commands/MusicRegionCommand.java
@@ -2,12 +2,9 @@ package com.mcmiddleearth.mcmemusic.commands;
 
 import com.mcmiddleearth.mcmemusic.Main;
 import com.mcmiddleearth.mcmemusic.Permission;
-import com.mcmiddleearth.mcmemusic.data.Region;
 import com.mcmiddleearth.mcmemusic.util.CreateRegion;
 import com.mcmiddleearth.mcmemusic.util.LoadRegion;
-import com.sk89q.worldedit.math.BlockVector3;
-import com.sk89q.worldedit.regions.Polygonal2DRegion;
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,23 +12,14 @@ import org.bukkit.entity.Player;
 
 public class MusicRegionCommand implements CommandExecutor {
 
-    private CreateRegion createRegion;
-    private LoadRegion loadRegion;
+    private final CreateRegion createRegion;
+    private final LoadRegion loadRegion;
+    private final Main main;
 
-    public MusicRegionCommand(CreateRegion createRegion, LoadRegion loadRegion){
+    public MusicRegionCommand(CreateRegion createRegion, LoadRegion loadRegion, Main main){
         this.createRegion = createRegion;
         this.loadRegion = loadRegion;
-    }
-
-    public void containCheck(Player p, Polygonal2DRegion region, Region musicRegion){
-        int x, z;
-        x = p.getLocation().getBlockX();
-        z = p.getLocation().getBlockZ();
-        BlockVector3 playerVector = BlockVector3.at(x, 0, z);
-
-        if(region.contains(playerVector)){
-            p.sendMessage("You are in a region with music ID of " + musicRegion.getMusicID());
-        }
+        this.main = main;
     }
 
     @Override
@@ -44,19 +32,19 @@ public class MusicRegionCommand implements CommandExecutor {
                     p.sendMessage("No permission");
                     return true;
                 }
+
                 if(args.length == 0 || args[0].equalsIgnoreCase("info")){
-                    sender.sendMessage("Command: /music create|delete <name> <musicID>");
+                    sender.sendMessage(ChatColor.RED + "Command: /music on|off|create|delete <name> <music ID> <weight>");
                     return true;
-                } else if(args[0].equalsIgnoreCase("off")) {
-                    Main.getInstance().getLoadRegion().getRegionsMap().forEach((region,musicRegion)-> {
-                        Main.getInstance().getPlayMusic().stopMusic(musicRegion, p);
-                    });
+                }
+                else if(args[0].equalsIgnoreCase("off")) {
                     Main.getInstance().getPlayerManager().deafen(p);
-                    p.sendMessage("MCME music disabled.");
+                    p.sendMessage(ChatColor.RED + "MCME music disabled.");
                     return true;
-                } else if(args[0].equalsIgnoreCase("on")) {
+                }
+                else if(args[0].equalsIgnoreCase("on")) {
                     Main.getInstance().getPlayerManager().undeafen(p);
-                    p.sendMessage("MCME music enabled.");
+                    p.sendMessage(ChatColor.GREEN + "MCME music enabled.");
                     return true;
                 }
 
@@ -65,24 +53,24 @@ public class MusicRegionCommand implements CommandExecutor {
                     p.sendMessage("No permission");
                     return true;
                 }
+
                 if(args[0].equalsIgnoreCase("create")){
                     try {
-                        createRegion.regionCreate(p, args[1], Integer.parseInt(args[2]));
+                        createRegion.regionCreate(p, args[1], Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+                        loadRegion.getPolyRegionsMap().clear();
+                        loadRegion.getCubeRegionsMap().clear();
                         loadRegion.loadRegions();
+                        p.sendMessage(ChatColor.GREEN + "Regions have been reloaded");
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    sender.sendMessage(Color.GREEN + "Region Created.");
-                    return true;
-                }
-                else if(args[0].equalsIgnoreCase("test")){
-                    loadRegion.getRegionsMap().forEach((k, v) -> containCheck(p, k, v));
+                    sender.sendMessage(ChatColor.GREEN + "Region Created.");
                     return true;
                 }
                 else if(args[0].equalsIgnoreCase("reload")){
-                    Main.getInstance().reloadConfig();
                     try {
-                        loadRegion.getRegionsMap().clear();
+                        loadRegion.getPolyRegionsMap().clear();
+                        loadRegion.getCubeRegionsMap().clear();
                         loadRegion.loadRegions();
                         p.sendMessage(ChatColor.GREEN + "Regions have been reloaded");
                     } catch (Exception e) {
@@ -90,6 +78,7 @@ public class MusicRegionCommand implements CommandExecutor {
                     }
                     return true;
                 }
+                sender.sendMessage(ChatColor.RED + "Command: /music on|off|create|delete <name> <music ID> <weight>");
             }
         }
         return false;
