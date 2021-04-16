3871d55fc48f7f4b76cd244bc49deca2d4510686
GilanRanger <58899528+GilanRanger@users.noreply.github.com>
2021-04-15 20:41:33 -0700

    Added /music play and /music stop

--- src/main/java/com/mcmiddleearth/mcmemusic/commands/MusicRegionCommand.java
+++ src/main/java/com/mcmiddleearth/mcmemusic/commands/MusicRegionCommand.java
@@ -47,6 +47,59 @@ public class MusicRegionCommand implements CommandExecutor {
                     p.sendMessage(ChatColor.GREEN + "MCME music enabled.");
                     return true;
                 }
+                else if(args[0].equalsIgnoreCase("play")){
+                    if(playerListening.containsKey(p)){
+                        int id = playerListening.get(p);
+
+                        ConfigurationSection path = main.getConfig().getConfigurationSection(String.valueOf(id));
+                        String soundFile = path.getString("file");
+                        if(soundFile!=null && !soundFile.contains(":")) {
+                            p.stopSound(Sound.valueOf(soundFile));
+                        } else {
+                            p.stopSound(soundFile);
+                        }
+
+                        p.sendMessage(ChatColor.GREEN + "Stopped Music.");
+                    }
+                    try{
+                        ConfigurationSection section = main.getConfig().getConfigurationSection(args[1]);
+                        int id = section.getInt("id");
+
+                        ConfigurationSection path = main.getConfig().getConfigurationSection(String.valueOf(id));
+
+                        String soundFile = path.getString("file");
+                        String name = path.getString("name");
+                        String link = path.getString("link");
+
+                        if(soundFile!=null && !soundFile.contains(":")) {
+                            p.playSound(p.getLocation(), Sound.valueOf(soundFile), 10000, 1);
+                        } else {
+                            p.playSound(p.getLocation(), soundFile, 10000, 1);
+                        }
+                        p.sendMessage(ChatColor.GREEN + "Playing " + ChatColor.ITALIC + name + ChatColor.RESET +
+                                ChatColor.GREEN + " [" + ChatColor.GRAY + link + ChatColor.GREEN + "]");
+
+                        playerListening.put(p, id);
+
+                    } catch(NullPointerException e){
+                        p.sendMessage(ChatColor.RED + "That song doesn't exist!");
+                    }
+                    return true;
+                }
+                else if(args[0].equalsIgnoreCase("stop")){
+                    int id = playerListening.get(p);
+
+                    ConfigurationSection path = main.getConfig().getConfigurationSection(String.valueOf(id));
+                    String soundFile = path.getString("file");
+                    if(soundFile!=null && !soundFile.contains(":")) {
+                        p.stopSound(Sound.valueOf(soundFile));
+                    } else {
+                        p.stopSound(soundFile);
+                    }
+
+                    p.sendMessage(ChatColor.GREEN + "Stopped Music.");
+                    return true;
+                }
 
                 //manager commands
                 if(!p.hasPermission(Permission.MANAGE.getNode())) {
